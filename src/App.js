78698b76349c9f5af0013537ee97c693e5fbd9e6
Vivek Pattanaik <vivekpatanaik100@gmail.com>
2018-12-19 23:43:03 +0530

    nav-bar added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,41 @@ import CreateToDo from "./components/create-todo.component";
 import EditToDo from "./components/edit-todo.component";
 import ToDoList from "./components/todo-list.component";
 
+import logo from "./logo.png";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack To-Do App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              className="navbar-brand"
+              href="https://github.com/vivekptnk"
+              target="_blank"
+            >
+              <img src={logo} width="50" height="50" alt="todologo" />
+            </a>
+
+            <Link to="/" className="navbar-brand">
+              To-Do App
+            </Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
           <Route path="/" exact component={ToDoList} />
           <Route path="/edit/:id" component={EditToDo} />
           <Route path="/create" component={CreateToDo} />
--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,6 +1,29 @@
 import React, { Component } from "react";
 
 export default class CreateToDo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
